dee8028077e5e9fac0900451c4dc5577f649eb2f
vbevia <vbevia@gmail.com>
2024-10-14 11:05:18 +0200

    clean code

--- kivyDesktopAgent.py
+++ kivyDesktopAgent.py
@@ -1,13 +1,13 @@
-import sys
 import subprocess
+import sys
 import threading
+
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.core.clipboard import Clipboard
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-from kivy.core.clipboard import Clipboard
-from kivy.clock import Clock
 
 
 class MyApp(App):
